46b4ae1c4573095f1fadd463a896b8789a95a2b3
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2024-06-28 22:30:35 +0100

    feat: verbosity for arguments

--- QT_StirTrace_v4.0/main.cpp
+++ QT_StirTrace_v4.0/main.cpp
@@ -133,81 +133,108 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    printf("Arguments:\n");
+
     if (arguments.count("d")) {
         FilePath = QString::fromStdString(arguments["d"].as<std::string>());
+        printf("\tPath: %s\n", FilePath.toUtf8().constData());
     }
     if (arguments.count("b")) {
         batchMode = true;
+        printf("\tBatch Mode\n");
     }
 
     if (arguments.count("n")) {
         NoiseLevels = QString::fromStdString(arguments["n"].as<std::string>());
+        printf("\tNoise Levels: %s\n", NoiseLevels.toUtf8().constData());
     }
     if (arguments.count("p")) {
         SaltAndPepper = QString::fromStdString(arguments["p"].as<std::string>());
+        printf("\tSalt and Pepper: %s\n", SaltAndPepper.toUtf8().constData());
     }
     if (arguments.count("g")) {
         GaussianNoise = QString::fromStdString(arguments["g"].as<std::string>());
+        printf("\tGaussian Noise: %s\n", GaussianNoise.toUtf8().constData());
     }
     if (arguments.count("m")) {
         MedianFilters = QString::fromStdString(arguments["m"].as<std::string>());
+        printf("\tMedian Filters: %s\n", MedianFilters.toUtf8().constData());
     }
     if (arguments.count("l")) {
         LineRemoval = QString::fromStdString(arguments["l"].as<std::string>());
+        printf("\tLine Removal: %s\n", LineRemoval.toUtf8().constData());
     }
     if (arguments.count("c")) {
         ColumnRemoval = QString::fromStdString(arguments["c"].as<std::string>());
+        printf("\tColumn Removal: %s\n", ColumnRemoval.toUtf8().constData());
     }
     if (arguments.count("r")) {
         RotAngles = QString::fromStdString(arguments["r"].as<std::string>());
+        printf("\tRotation: %s\n", RotAngles.toUtf8().constData());
     }
     if (arguments.count("x")) {
         XStretchFactors = QString::fromStdString(arguments["x"].as<std::string>());
+        printf("\tStretching in X Direction: %s\n", XStretchFactors.toUtf8().constData());
     }
     if (arguments.count("y")) {
         YShearFactors = QString::fromStdString(arguments["y"].as<std::string>());
+        printf("\tShearing in Y Direction: %s\n", YShearFactors.toUtf8().constData());
     }
     if (arguments.count("s")) {
         ScaleFactors = QString::fromStdString(arguments["s"].as<std::string>());
+        printf("\tScaling: %s\n", ScaleFactors.toUtf8().constData());
     }
     if (arguments.count("o")) {
         CropFactors = QString::fromStdString(arguments["o"].as<std::string>());
+        printf("\tCropping: %s\n", CropFactors.toUtf8().constData());
     }
     if (arguments.count("a")) {
         BandingFrequency = QString::fromStdString(arguments["a"].as<std::string>());
+        printf("\tBanding Artifacts: %s\n", BandingFrequency.toUtf8().constData());
     }
     if (arguments.count("f")) {
         ShiftParams = QString::fromStdString(arguments["f"].as<std::string>());
+        printf("\tShift Values: %s\n", ShiftParams.toUtf8().constData());
     }
     if (arguments.count("i")) {
         TiltParams = QString::fromStdString(arguments["i"].as<std::string>());
+        printf("\tTilt Simulation: %s\n", TiltParams.toUtf8().constData());
     }
     if (arguments.count("8")) {
         eightBitConversion = true;
+        printf("\t8 bit conversion\n");
     }
     if (arguments.count("e")) {
         SubstrateFile = QString::fromStdString(arguments["e"].as<std::string>());
+        printf("\tEmbed Trace on Substrate: %s\n", SubstrateFile.toUtf8().constData());
     }
     if (arguments.count("t")) {
         combineMode = true;
+        printf("\tCombine Filtering Techniques\n");
     }
     if (arguments.count("eval")) {
         EvalMode = QString::fromStdString(arguments["eval"].as<std::string>());
+        printf("\tEvaluation: %s\n", EvalMode.toUtf8().constData());
     }
     if (arguments.count("preprocess")) {
         PreProcessMode = QString::fromStdString(arguments["preprocess"].as<std::string>());
+        printf("\tPreprocessing: %s\n", PreProcessMode.toUtf8().constData());
     }
     if (arguments.count("baseline")) {
         baseline = true;
+        printf("\tDetermine Baseline Performance\n");
     }
     if (arguments.count("passport")) {
         passportscaling = QString::fromStdString(arguments["passport"].as<std::string>());
+        printf("\tPassport Resizing: %s\n", passportscaling.toUtf8().constData());
     }
     if (arguments.count("doublescale")) {
         DoubleScalingFactors = QString::fromStdString(arguments["doublescale"].as<std::string>());
+        printf("\tDouble Scaling: %s\n", DoubleScalingFactors.toUtf8().constData());
     }
     if (arguments.count("haarcascade")) {
         haarcascade = QString::fromStdString(arguments["haarcascade"].as<std::string>());
+        printf("\tHaar Cascade: %s\n", haarcascade.toUtf8().constData());
     }
 
     if ((batchMode) || (!useGUI)) {
